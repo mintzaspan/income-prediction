89dc0b825d769bf6ef20af66224c5203af5b0e37
Panagiotis Mintzas <150053156+mintzaspan@users.noreply.github.com>
2025-02-07 17:13:41 +0200

    use config

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,7 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
 import os
+import json
 
 # fetch dataset
 census_income = fetch_ucirepo(id=20)
@@ -12,8 +13,12 @@ y = census_income.data.targets
 # build dataset
 df = pd.merge(left=X, right=y, left_index=True, right_index=True)
 
+# import env variables
+with open("config.json", "r") as f:
+    config = json.load(f)
+
 # check if data folder exists
-data_dir = "data"
+data_dir = os.path.dirname(config["raw_data"])
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 
